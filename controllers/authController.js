fc0b94a8709ef447e080e191a52d8d4ca27f5975
WanjiruKagema <maureenkagema@gmail.com>
2022-03-09 17:22:47 +0300

    added timestamps and token to register API

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,20 +5,24 @@ const bcrypt = require('bcryptjs');
 
 exports.signup = async (req, res) => {
 	try {
+        //get user input
 		const { first_name, last_name, email, phone_number, password } = req.body
-
+        //validate user input
 		if (!(email && password && first_name && last_name && phone_number)) {
 			res.status(400).send("All inputs are required")
 		}
-
+        
+        //check if user exists
 		const oldUser = await User.findOne({ email })
 
 		if (oldUser) {
 			return res.status(409).send("User already exists, Please Log In")
 		}
-
+       
+        //encrypt password
 		encryptedPassword = await bcrypt.hash(password, 10)
 
+        //create user in db
 		const user = await User.create({
 			first_name,
 			last_name,
@@ -27,6 +31,7 @@ exports.signup = async (req, res) => {
 			password: encryptedPassword,
 		})
 
+        //create token
 		const token = jwt.sign(
 			{ user_id: user._id, email },
 			process.env.JWT_SECRET,
@@ -35,6 +40,7 @@ exports.signup = async (req, res) => {
 			}
 		)
 
+        //save the token
 		user.token = token
 
 		res.cookie("token", token, {
@@ -43,7 +49,8 @@ exports.signup = async (req, res) => {
 
 		console.log("token from cookie from server is ", res.cookie.token)
 		console.log("token from server is ", token)
-
+       
+        //return new user
 		res.status(201).json(user)
 	} catch (err) {
 		console.log("error",err)
--- models/User.js
+++ models/User.js
@@ -30,7 +30,7 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    tokens: {
+    token: {
         type: String,
     }
    
