1f7dfd4d66ed16d2e33eb162c194ee2f3327396d
Caleno-R <calenorose@gmail.com>
2022-03-10 09:48:14 +0300

    feat: added cookie parser

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require("dotenv").config()
 require("./config/db").connect()
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const cookieParser = require('cookie-parser')
+
+const app = express()
 const cors = require("cors")
+
 app.use( cors())
 app.use(express.json())
+app.use(cookieParser())
 
 
 app.use("/auth", require('./routes/authRoute'))
