61fb423a06e6dc890af4c45114627dc2d4ad8a07
IshaqKassam <ishaq.kassam@gmail.com>
2022-03-25 00:22:07 +0300

    ready to present'

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/User")
 exports.findAll = (req, res) => {
   User.find()
     .then((users) => {
-      res.status(200).json(users)
+      res.status(200).send(users)
     })
     .catch((err) => {
       res.status(500).json({
